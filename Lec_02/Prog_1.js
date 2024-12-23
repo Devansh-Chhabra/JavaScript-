ad668bc3c72626003486b7394a4356ec90ec263c
Devansh-Chhabra <142912156+Devansh-Chhabra@users.noreply.github.com>
2024-12-23 12:27:55 +0000

    data-types

---
+++ Lec_02/Prog_1.js
@@ -0,0 +1,18 @@
+// Various Data-types
+
+let value = 23      // number
+let names = "Devansh"   // string
+let ans = true;     // bool
+let state = null;   // null
+let age = undefined;    // let age;...this is by default undefined
+
+// typeof Operator
+console.log(typeof age);
+console.log(typeof state);
+console.log(typeof names);
+
+/* 
+Some other data types
+-> symbol : for uniqueness
+-> bigint : to extend the range of numbers
+*/
