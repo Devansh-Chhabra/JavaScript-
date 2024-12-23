aeb15d1a99f8744ec79737826d2e4de5d2fc637e
Devansh-Chhabra <142912156+Devansh-Chhabra@users.noreply.github.com>
2024-12-23 11:38:21 +0000

    done

--- Lec_01/Prog_1.js
+++ Lec_01/Prog_1.js
@@ -1,1 +1,18 @@
-console.log("Devansh Chhabra")
+// Variables and Constants
+const accountId = 144553
+let accountEmail = "devanshchhabr@gmail.com"
+var accountPassword = "#Devansh@13"
+accountCity = "Jaipur"
+
+/* 
+-> const values cannot be changed
+-> let and var look similar but difference lies in the scope...block scope and functional scope
+-> we can create the variables without mentioning the keywords which is considered a bad practice
+*/
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+// if we do not define the variable then its default value is "undefined"
+let accountState
+console.log(accountState)
