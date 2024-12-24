7e92bd27f67e39707a15a57f03c409f3c5a4e50a
Devansh-Chhabra <142912156+Devansh-Chhabra@users.noreply.github.com>
2024-12-24 11:30:03 +0000

    changes done

---
+++ Lec_03/Prog_1.js
@@ -0,0 +1,61 @@
+// Data-type Conversions
+
+// To Number
+let score = "33"    
+let toNumber = Number(score);
+
+console.table([typeof score,typeof toNumber,score,toNumber])
+
+score = null
+toNumber = Number(score)
+console.table([typeof score,typeof toNumber,score,toNumber])
+
+score = undefined
+toNumber = Number(score)
+console.table([typeof score,typeof toNumber,score,toNumber])
+
+score = true
+toNumber = Number(score)
+console.table([typeof score,typeof toNumber,score,toNumber])
+
+
+
+
+
+// To Boolean
+let loggedIn = 100
+let toBool = Boolean(loggedIn)
+console.table([typeof loggedIn,typeof toBool,loggedIn,toBool])
+
+loggedIn = null
+toBool = Boolean(loggedIn)
+console.table([typeof loggedIn,typeof toBool,loggedIn,toBool])
+
+loggedIn = undefined
+toBool = Boolean(loggedIn)
+console.table([typeof loggedIn,typeof toBool,loggedIn,toBool])
+
+loggedIn = "Devansh"
+toBool = Boolean(loggedIn)
+console.table([typeof loggedIn,typeof toBool,loggedIn,toBool])
+
+
+
+
+
+// To string
+let value = 33
+let toString = String(value)
+console.table([typeof value,typeof toString,value,toString])
+
+value = true
+toString = String(value)
+console.table([typeof value,typeof toString,value,toString])
+
+value = null
+toString = String(value)
+console.table([typeof value,typeof toString,value,toString])
+
+value = undefined
+toString = String(value)
+console.table([typeof value,typeof toString,value,toString])
---
+++ Lec_03/Prog_2.js
@@ -0,0 +1,22 @@
+// Basics
+console.log(2+2)
+console.log(2-2)
+console.log(2*3)
+console.log(4/1)
+console.log(2**5)
+
+let gameCounter = 100
+console.log(gameCounter++)
+
+
+
+
+// Some confusing Conversions
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+"2"+2)
+console.log(1+2+"2")
+
+console.log(true)
+console.log(+true)
